1efe51f5ed76682f82ef037249a09a9bad4ee53f
NguyenTuan <Tuan94pth@gmail.com>
2023-05-10 00:48:39 +0700

    homepage ui

---
+++ src/shares/content/ContentNew.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./content.css";
+
+const ContentNew = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="search__container">
+          <p className="search__title">NETFARM - Sản xuất vụ mùa đảm bảo trong khung thời vụ tốt nhất</p>
+          <input className="search__input" type="text" placeholder="Tìm kiếm" />
+        </div>
+        <div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContentNew;
---
+++ src/shares/navbar/NavBar.jsx
@@ -0,0 +1,64 @@
+import { CloudFilled, MailFilled } from "@ant-design/icons";
+import { Button, Space } from "antd";
+import React from "react";
+import logo from "../../assets/image/logonetfarm.png";
+import "./navbar.css";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div id="main-navbar" className="navbar">
+        <img src={logo} style={{ height: "60px" }} />
+        <div style={{ textAlign: "center", color: "black" }}>
+          <CloudFilled
+            style={{ color: "#00CCFF", fontSize: 40, paddingTop: 30 }}
+          />
+          <h2 style={{ color: "white" }}>Da Nang, 30°C</h2>
+        </div>
+        <div
+          style={{
+            alignItems: "center",
+            textAlign: "center",
+            alignContent: "center",
+            display: "flex",
+            justifyContent: "center",
+            color: "black",
+          }}
+        >
+          <MailFilled
+            style={{ color: "#FFF", fontSize: 20, paddingRight: 10 }}
+          />
+          <p style={{ color: "#FFF", fontSize: 20 }}>needhelp@company.com</p>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="/home">Trang chủ</a>
+            </li>
+            <li>
+              <a href="/home">Thông tin</a>
+            </li>
+            <li>
+              <a href="/home">Mua hàng</a>
+            </li>
+            <li>
+              <a href="/home">Nhận diện cây trồng</a>
+            </li>
+            <li>
+              <Space wrap>
+                <Button type="primary" shape="round" size="medium" danger>
+                  Đăng nhập
+                </Button>
+                <Button type="primary" shape="round" size="medium" danger>
+                  Đăng ký
+                </Button>
+              </Space>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
